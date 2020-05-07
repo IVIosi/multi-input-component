d32ce11e6a2ca3db32f5b09a465076ef0e9101b3
Mostafa <mos.sameti@gmail.com>
2020-05-08 04:05:46 +0430

    add main component logic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function App(params) {
-  return <p>sss</p>;
+import InputRow from './components/input-row';
+
+export default function App({ onChange }) {
+  const [inputFields, setInputFields] = useState({
+    '1': '',
+  });
+  const addFieldRow = () => {
+    setInputFields((prevState) => {
+      const arrOfKeys = Object.keys(prevState);
+      const lastFieldKey = arrOfKeys[arrOfKeys.length - 1];
+      return {
+        ...prevState,
+        [Number(lastFieldKey) + 1]: '',
+      };
+    });
+  };
+  const handleSubmit = () => {
+    const inputValues = Object.values(inputFields);
+    inputValues.pop();
+    onChange(inputValues);
+  };
+  const handleFieldInputChange = (value, fieldKey) => {
+    setInputFields((prevState) => ({
+      ...prevState,
+      [fieldKey]: value,
+    }));
+  };
+  const handleFieldRemove = (fieldKey) => {
+    setInputFields((prevState) => {
+      let { [fieldKey]: omit, ...newState } = prevState;
+      return newState;
+    });
+  };
+
+  return (
+    <>
+      {Object.keys(inputFields).map((fieldKey, index) => {
+        const isLastField = index === Object.keys(inputFields).length - 1;
+        return (
+          <InputRow
+            key={fieldKey}
+            isLastField={isLastField}
+            onChange={(value) => handleFieldInputChange(value, fieldKey)}
+            onRemove={() => handleFieldRemove(fieldKey)}
+            onInput={isLastField ? addFieldRow : null}
+          />
+        );
+      })}
+      <button onClick={handleSubmit}>submit</button>
+    </>
+  );
 }
