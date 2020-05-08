c1a1948161dda3436510c9543f59dea1f7619fac
Mostafa <mos.sameti@gmail.com>
2020-05-08 16:35:44 +0430

    styling input row componet

--- src/components/input-row.js
+++ src/components/input-row.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function InputRow({ onInput, onChange, onRemove, isLastField }) {
+export default function InputRow(props) {
+  const { label, onInput, onChange, onRemove, isLastField } = props;
+  const [active, setActive] = useState(false);
   const handleInput = (e) => {
     if (typeof onInput === 'function') {
       onInput();
     }
     onChange(e.target.value);
   };
+  const handleFocus = (e) => {
+    setActive(true);
+  };
+  const handleBlur = (e) => {
+    setActive(false);
+  };
   return (
-    <>
-      <input onInput={handleInput} />
-      {!isLastField && <button onClick={onRemove}>remove</button>}
-    </>
+    <div className="field-input">
+      <div className={'field-input__text'.concat(active ? ' active' : '')}>
+        <label className="field-input__label">{label}</label>
+        <input
+          className="field-input__input"
+          onInput={handleInput}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+      </div>
+      {!isLastField && (
+        <button
+          className="field-input__remove-button"
+          onClick={onRemove}
+        ></button>
+      )}
+    </div>
   );
 }
