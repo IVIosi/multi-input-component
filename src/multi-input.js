c963ccbe3d167b504528125234840b95ef51f83a
Mostafa <mos.sameti@gmail.com>
2020-05-08 16:36:25 +0430

    styling fields and buttons

--- src/multi-input.js
+++ src/multi-input.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import InputRow from './components/input-row';
 
-export default function App({ onChange }) {
+export default function MultiInput({ name, title, onChange }) {
   const [inputFields, setInputFields] = useState({
     '1': '',
   });
@@ -35,20 +35,29 @@ export default function App({ onChange }) {
   };
 
   return (
-    <>
-      {Object.keys(inputFields).map((fieldKey, index) => {
-        const isLastField = index === Object.keys(inputFields).length - 1;
-        return (
-          <InputRow
-            key={fieldKey}
-            isLastField={isLastField}
-            onChange={(value) => handleFieldInputChange(value, fieldKey)}
-            onRemove={() => handleFieldRemove(fieldKey)}
-            onInput={isLastField ? addFieldRow : null}
-          />
-        );
-      })}
-      <button onClick={handleSubmit}>submit</button>
-    </>
+    <div className="main-form">
+      <div className="multi-input">
+        <p>{title}</p>
+        {Object.keys(inputFields).map((fieldKey, index) => {
+          const isLastField = index === Object.keys(inputFields).length - 1;
+          return (
+            <InputRow
+              label={name}
+              key={fieldKey}
+              isLastField={isLastField}
+              onChange={(value) => handleFieldInputChange(value, fieldKey)}
+              onRemove={() => handleFieldRemove(fieldKey)}
+              onInput={isLastField ? addFieldRow : null}
+            />
+          );
+        })}
+      </div>
+      <div className="button-section">
+        <button className="btn secondary">CANCEL</button>
+        <button className="btn primary" onClick={handleSubmit}>
+          SAVE
+        </button>
+      </div>
+    </div>
   );
 }
